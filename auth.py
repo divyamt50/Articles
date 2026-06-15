593e48a38be305cf740700ac7def985b02265d7c
divyamt50 <divyamt50@gmail.com>
2026-06-15 09:07:06 +0530

    added bcrypt to hash passwords

--- auth.py
+++ auth.py
@@ -1,55 +1,49 @@
 import os
-from datetime import timedelta, datetime, timezone
-
-import bcrypt
 import jwt
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from database import get_session
 from models import User
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone
+import bcrypt
+from fastapi import Depends, status, HTTPException
+from database import get_session
 
-
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "JWT_SECRET_KEY")
-JWT_ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTE = 30
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "secret_key")
+JWT_EXPIRE_TIME = 60
+JWT_ALGORITHM = "HSA256"
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
-#password hashing
 def hash_password(plain:str)->str:
     return bcrypt.hashpw(plain.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
-#password verification
-def password_verify(plain:str, hashed:str)->bool:
-    return bcrypt.checkpw(plain.encode("utf-8"), hashed.encode("utf-8"))
+def verify_password(plain:str, hash:str)->bool:
+    return bcrypt.checkpw(plain.encode("utf-8"), hash.encode("utf-8"))
 
-# create token
-def create_access_token(user_id:int)->str:
-    expire_time = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTE)
-    payload = {"sub":str(user_id), "exp":expire_time}
-    return jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+def generate_jwt_token(user_id:int)->str:
+    expire_time = datetime.now(timezone.utc) + timedelta(minutes=JWT_EXPIRE_TIME)
+    payload = {"sub": str(user_id), "exp":expire_time}
+    return jwt.encode(payload, JWT_SECRET_KEY, expire_time)
 
 async def get_current_user(
-        token:str = Depends(oauth2_scheme),
-        session:AsyncSession = Depends(get_session)
+        token:str=Depends(oauth2_scheme),
+        session:AsyncSession=Depends(get_session)
 )->User:
-    credentials_error = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="could not validate credentials",
+    credential_error = HTTPException(
+        status_code= status.HTTP_401_UNAUTHORIZED,
+        detail="unauthorized access",
         headers={"WWW-Authenticate":"Bearer"}
     )
 
     try:
-        payload = jwt.decode(token, JWT_SECRET_KEY, JWT_ALGORITHM)
-        user_id = payload.get("sub")
+        res = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        user_id = int(res.get("sub"))
         if user_id is None:
-            raise credentials_error
+            raise credential_error
     except jwt.PyJWTError:
-        raise credentials_error
+        raise credential_error
     
-    user = await session.get(User, int(user_id))
-    if user is None:
-        raise credentials_error
+    user = await session.get(User, user_id)
+    if not user:
+        raise credential_error
     return user
